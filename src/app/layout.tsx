c9ec5748020072992f4a44d994894ae770c5990e
Ayomide Oguntuase <oguntuase3@gmail.com>
2024-06-03 10:42:30 +0100

    fix: updated favicons, readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,41 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Zwilt Design by Ayo Oguntuase",
   description: "Punch Zwilt Submission",
 };
 
-const switzer = localFont({src: [
-  {
-    path: './fonts/Switzer-Regular.otf',
-    weight: '400',
-    style: 'normal',
-  },
-  {
-    path: './fonts/Switzer-Medium.otf',
-    weight: '500',
-    style: 'normal',
-  },
-  {
-    path: './fonts/Switzer-Semibold.otf',
-    weight: '600',
-    style: 'normal',
-  },
-  {
-    path: './fonts/Switzer-Bold.otf',
-    weight: '700',
-    style: 'normal',
-  },
-  {
-    path: './fonts/Switzer-BoldItalic.otf',
-    weight: '700',
-    style: 'italic',
-  },
-]})
-
+const switzer = localFont({
+  src: [
+    {
+      path: "./fonts/Switzer-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Switzer-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Switzer-Semibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Switzer-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Switzer-BoldItalic.otf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -43,6 +44,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={switzer.className}>{children}</body>
     </html>
   );
